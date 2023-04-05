de125fe2426bd061e7c20997bd4ebb22d7f1583e
Naveena-500 <naveena.vemula@500apps.com>
2023-04-05 20:36:37 +0530

    Task implementation

---
+++ test/components/collections/Main.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="bg-white p-4 sm:px-6">
+    <CollectionsList
+      :tasks="tasks"
+      @edit-task="editTask"
+      @delete-task="deleteTask"
+    ></CollectionsList>
+    <div>
+      <button
+        @click="openModel"
+        type="button"
+        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Create a new task
+      </button>
+      <TransitionRoot as="template" :show="open">
+        <Dialog as="div" class="relative z-10" @close="open = false">
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0"
+            enter-to="opacity-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100"
+            leave-to="opacity-0"
+          >
+            <div
+              class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            />
+          </TransitionChild>
+
+          <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div
+              class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+            >
+              <TransitionChild
+                as="template"
+                enter="ease-out duration-300"
+                enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enter-to="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leave-from="opacity-100 translate-y-0 sm:scale-100"
+                leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <DialogPanel
+                  class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                >
+                  <CollectionsAdd
+                    @hide="closeModel"
+                    @save-task="saveTask"
+                  ></CollectionsAdd>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </TransitionRoot>
+      <TransitionRoot as="template" :show="editModel">
+        <Dialog as="div" class="relative z-10" @close="editModel = false">
+          <TransitionChild
+            as="template"
+            enter="ease-out duration-300"
+            enter-from="opacity-0"
+            enter-to="opacity-100"
+            leave="ease-in duration-200"
+            leave-from="opacity-100"
+            leave-to="opacity-0"
+          >
+            <div
+              class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+            />
+          </TransitionChild>
+
+          <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div
+              class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
+            >
+              <TransitionChild
+                as="template"
+                enter="ease-out duration-300"
+                enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enter-to="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leave-from="opacity-100 translate-y-0 sm:scale-100"
+                leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <DialogPanel
+                  class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6"
+                >
+                  <CollectionsEdit
+                    v-if="editModel"
+                    :editData="editData"
+                    :editModel="editModel"
+                    @hide="closeModel"
+                    @update-task="updateTask"
+                  ></CollectionsEdit>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
+        </Dialog>
+      </TransitionRoot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  TransitionChild,
+  TransitionRoot,
+} from "@headlessui/vue";
+import {
+  CodeBracketIcon,
+  EllipsisVerticalIcon,
+  FlagIcon,
+  StarIcon,
+} from "@heroicons/vue/20/solid";
+
+interface CollectionSchema {
+  project_id: String;
+  entityId: String;
+  entity: String;
+  url: String;
+  ownerID: string;
+  category: string;
+  status: String;
+  description: String;
+  isActive: string;
+  due_date: String;
+  getUrl: string;
+}
+const props = withDefaults(defineProps<CollectionSchema>(), {
+  project_id: "1",
+  entityId: "2",
+  entity: "CONTACTS",
+  ownerID: "1",
+  category: "COMPLETED",
+  status: "NEW",
+  description: "Completed Tasks",
+  isActive: "ACTIVE",
+  due_date: "2023-04-05",
+});
+let editModel = ref(false);
+let editData = ref({});
+let open = ref(false);
+
+const { pending, data: tasks } = useAuthLazyFetch(props.getUrl, {});
+const openModel = () => {
+  open.value = true;
+};
+const closeModel = (e: any) => {
+  editModel.value = false;
+  open.value = false;
+  open.value = e;
+};
+const saveTask = async (task: string) => {
+  console.log("task----", task);
+  const { data } = await useAuthLazyFetchPost(
+    `${props.url}/${props.entity}/${props.entityId}`,
+    {
+      body: {
+        project_id: props.project_id,
+        name: task,
+        entity: props.entity,
+        entity_id: props.entityId,
+        owner_id: props.ownerID,
+        category: props.category,
+        status: props.status,
+        description: props.description,
+        is_active: props.isActive,
+        due_date: props.due_date,
+        kanban_order: [],
+      },
+    }
+  );
+  console.log("nbczxmnc===", data.value);
+  tasks.value.push(data.value);
+};
+const editTask = (data: object) => {
+  editModel.value = true;
+  open.value = false;
+  editData.value = data;
+};
+const updateTask = async (editedItem: any, name: string) => {
+  useAuthLazyFetchPut(`${props.url}/${editedItem.uid}`, {
+    body: {
+      project_id: props.project_id,
+      name: name,
+      entity: props.entity,
+      entity_id: props.entityId,
+      owner_id: props.ownerID,
+      category: props.category,
+      status: props.status,
+      description: props.description,
+      is_active: props.isActive,
+      due_date: props.due_date,
+      kanban_order: [],
+    },
+  });
+  let taskIndex = tasks.value.findIndex(
+    (data: any) => data.uid === editedItem.uid
+  );
+  if (taskIndex != -1) tasks.value[taskIndex].name = name;
+};
+const deleteTask = (uid: any) => {
+  useAuthLazyFetchDelete(`${props.url}/${uid}`, {});
+  let taskIndex = tasks.value.findIndex((data: any) => data.uid === uid);
+  if (taskIndex != -1) tasks.value.splice(taskIndex, 1);
+};
+</script>
---
+++ test/components/tasks.vue
@@ -0,0 +1,227 @@
+<template>
+  <div class="min-h-full bg-gray-100">
+    <div class="py-10">
+      <!-- grid section -->
+      <div class="px-6 lg:grid lg:grid-cols-5 lg:gap-8 lg:px-8">
+        <div class="mt-4">
+          <div
+            class="flex items-center justify-between border-2 rounded-md p-2 border-b-2 border-gray-100 border-b-indigo-500 bg-white"
+          >
+            <span class="p-1 border rounded-lg bg-indigo-50 cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 text-indigo-700"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                />
+              </svg>
+            </span>
+            <h1>Tasks</h1>
+            <span class="cursor-pointer">
+              <Menu as="div" class="relative inline-block text-left">
+                <div>
+                  <MenuButton
+                    class="flex items-center rounded-full bg-white text-gray-400 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+                  >
+                    <span class="sr-only">Open options</span>
+                    <EllipsisVerticalIcon class="h-5 w-5" aria-hidden="true" />
+                  </MenuButton>
+                </div>
+
+                <transition
+                  enter-active-class="transition ease-out duration-100"
+                  enter-from-class="transform opacity-0 scale-95"
+                  enter-to-class="transform opacity-100 scale-100"
+                  leave-active-class="transition ease-in duration-75"
+                  leave-from-class="transform opacity-100 scale-100"
+                  leave-to-class="transform opacity-0 scale-95"
+                >
+                  <MenuItems
+                    class="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  >
+                    <div class="py-1">
+                      <MenuItem v-slot="{ active }">
+                        <a
+                          href="#"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'group flex items-center px-4 py-2 text-sm',
+                          ]"
+                        >
+                          <PencilSquareIcon
+                            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                          />
+                          Edit
+                        </a>
+                      </MenuItem>
+                      <MenuItem v-slot="{ active }">
+                        <a
+                          href="#"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'group flex items-center px-4 py-2 text-sm',
+                          ]"
+                        >
+                          <DocumentDuplicateIcon
+                            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                          />
+                          Duplicate
+                        </a>
+                      </MenuItem>
+                    </div>
+                    <div class="py-1">
+                      <MenuItem v-slot="{ active }">
+                        <a
+                          href="#"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'group flex items-center px-4 py-2 text-sm',
+                          ]"
+                        >
+                          <ArchiveBoxIcon
+                            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                          />
+                          Archive
+                        </a>
+                      </MenuItem>
+                      <MenuItem v-slot="{ active }">
+                        <a
+                          href="#"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'group flex items-center px-4 py-2 text-sm',
+                          ]"
+                        >
+                          <ArrowRightCircleIcon
+                            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                          />
+                          Move
+                        </a>
+                      </MenuItem>
+                    </div>
+                    <div class="py-1">
+                      <MenuItem v-slot="{ active }">
+                        <a
+                          href="#"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'group flex items-center px-4 py-2 text-sm',
+                          ]"
+                        >
+                          <UserPlusIcon
+                            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                          />
+                          Share
+                        </a>
+                      </MenuItem>
+                      <MenuItem v-slot="{ active }">
+                        <a
+                          href="#"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'group flex items-center px-4 py-2 text-sm',
+                          ]"
+                        >
+                          <HeartIcon
+                            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                          />
+                          Add to favorites
+                        </a>
+                      </MenuItem>
+                    </div>
+                    <div class="py-1">
+                      <MenuItem v-slot="{ active }">
+                        <a
+                          href="#"
+                          :class="[
+                            active
+                              ? 'bg-gray-100 text-gray-900'
+                              : 'text-gray-700',
+                            'group flex items-center px-4 py-2 text-sm',
+                          ]"
+                        >
+                          <TrashIcon
+                            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+                            aria-hidden="true"
+                          />
+                          Delete
+                        </a>
+                      </MenuItem>
+                    </div>
+                  </MenuItems>
+                </transition>
+              </Menu>
+            </span>
+          </div>
+          <CollectionsMain :url="url" :getUrl="getUrl"></CollectionsMain>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Popover,
+  PopoverButton,
+  PopoverPanel,
+} from "@headlessui/vue";
+import {
+  ChatBubbleLeftEllipsisIcon,
+  CodeBracketIcon,
+  EllipsisVerticalIcon,
+  EyeIcon,
+  FlagIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+  ShareIcon,
+  StarIcon,
+} from "@heroicons/vue/20/solid";
+import {
+  ArrowTrendingUpIcon,
+  Bars3Icon,
+  BellIcon,
+  FireIcon,
+  HomeIcon,
+  UserGroupIcon,
+  XMarkIcon,
+  HandThumbDownIcon,
+  HandThumbUpIcon,
+  PhoneIcon,
+  CalendarIcon,
+  BriefcaseIcon,
+} from "@heroicons/vue/24/outline";
+
+const url = "https://v1-orm-lib.mars.hipso.cc/tasks";
+const getUrl =
+  "https://v1-orm-lib.mars.hipso.cc/tasks/CONTACTS/2?offset=0&limit=100&sort_column=id&sort_direction=desc";
+</script>
